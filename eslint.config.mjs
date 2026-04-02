0ee22168ac7e93c1388e69e873c12862b1e50e85
raju6991 <techwithrazu@gmail.com>
2026-04-03 08:11:29 +1000

    test husky 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig(
         },
         rules: {
             // 'no-console': 'error',
+            'dot-notation': 'error',
         },
     },
 )
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 function welcome(name: string) {
     console.log('hello')
-    return name
+
+    const user = {
+        name: 'raju',
+    }
+    const fname = user.name
+    return name + fname
 }
 
 welcome('Raju')
