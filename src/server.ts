c8645f1edde84f0ac6a04794ddddeb5c4f90b7da
raju6991 <techwithrazu@gmail.com>
2026-04-03 08:15:33 +1000

    add lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
 function welcome(name: string) {
-    console.log('hello')
-
-    const user = {
-        name: 'raju',
-    }
-    const fname = user.name
-    return name + fname
+    return name
 }
 
 welcome('Raju')
