ac3de97b5a89e2449911c04110ebed4acf63ccb2
raju6991 <techwithrazu@gmail.com>
2026-04-06 22:15:33 +1000

    Email required validation

---
+++ src/validator/register-validator.ts
@@ -0,0 +1,7 @@
+import { checkSchema } from 'express-validator'
+export default checkSchema({
+    email: {
+        errorMessage: 'Email is required!',
+        notEmpty: true,
+    },
+})
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,9 +1,10 @@
 import request from 'supertest'
 import app from '../../src/app'
 import { User } from '../../src/entity/User'
+import { Roles } from '../../src/constants'
 import { DataSource } from 'typeorm'
 import { AppDataSource } from '../../src/config/data-source'
-import { turncateTables } from '../utils'
+
 describe('POST /auth/register', () => {
     let connection: DataSource
 
@@ -13,7 +14,8 @@ describe('POST /auth/register', () => {
 
     beforeEach(async () => {
         // Database truncate
-        await turncateTables(connection)
+        await connection.dropDatabase()
+        await connection.synchronize()
     })
     afterAll(async () => {
         await connection.destroy()
@@ -94,5 +96,83 @@ describe('POST /auth/register', () => {
         const users = await repository.find()
         expect((response.body as Record<string, string>).id).toBe(users[0].id)
     })
-    describe('Fields are missing', () => {})
+
+    it('should assign a customer role', async () => {
+        //Arrange
+        const userData = {
+            firstName: 'Raju',
+            lastName: 'Lamsal',
+            email: 'rajulamsal@gmail.com',
+            password: 'secrete',
+        }
+        //Act
+        await request(app).post('/auth/register').send(userData)
+
+        //Assert
+        const userRepository = connection.getRepository(User)
+        const users = await userRepository.find()
+        expect(users[0]).toHaveProperty('role')
+        expect(users[0].role).toBe(Roles.CUSTOMER)
+    })
+
+    it('should store the hashed password in the database', async () => {
+        //Arrange
+        const userData = {
+            firstName: 'Raju',
+            lastName: 'Lamsal',
+            email: 'rajulamsal@gmail.com',
+            password: 'secrete',
+        }
+        //Act
+        await request(app).post('/auth/register').send(userData)
+
+        //Assert
+        const userRepository = connection.getRepository(User)
+        const users = await userRepository.find()
+        console.log(users[0].password)
+        expect(users[0].password).not.toBe(userData.password)
+        expect(users[0].password).toHaveLength(60)
+        expect(users[0].password).toMatch(/^\$2b\$\d+\$/)
+    })
+
+    it('should return 400 status code if email is already exists', async () => {
+        //Arrange
+        const userData = {
+            firstName: 'Raju',
+            lastName: 'Lamsal',
+            email: 'rajulamsal@gmail.com',
+            password: 'secrete',
+        }
+        const userRepository = connection.getRepository(User)
+        await userRepository.save({ ...userData, role: Roles.CUSTOMER })
+        //Act
+        const response = await request(app)
+            .post('/auth/register')
+            .send(userData)
+        const users = await userRepository.find()
+        //Assert
+        expect(response.statusCode).toBe(400)
+        expect(users).toHaveLength(1)
+    })
+
+    describe('Fields are missing', () => {
+        it('should return 400 status code if email field is missing', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'Raju',
+                lastName: 'Lamsal',
+                email: '',
+                password: 'secrete',
+            }
+            //Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+            //Assert
+            expect(response.statusCode).toBe(400)
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users).toHaveLength(0)
+        })
+    })
 })
