4309bb890d41ff23d74d62d18a4aa9ca2a0f7ab5
raju6991 <techwithrazu@gmail.com>
2026-04-11 10:28:03 +1000

    check user is authenticated while creating tenant

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -4,13 +4,18 @@ import { TenantService } from '../services/TenantService'
 import { AppDataSource } from '../config/data-source'
 import { Tenant } from '../entity/Tenant'
 import logger from '../config/logger'
+import authenticate from '../middlewares/authenticate'
 const router = express.Router()
 
 const tenantRepository = AppDataSource.getRepository(Tenant)
 const tenantService = new TenantService(tenantRepository)
 const tenantController = new TenantController(tenantService, logger)
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
-    tenantController.create(req, res, next)
-})
+router.post(
+    '/',
+    authenticate,
+    (req: Request, res: Response, next: NextFunction) => {
+        tenantController.create(req, res, next)
+    },
+)
 
 export default router
