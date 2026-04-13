55a6186073b35df7570e6f4fa8ec277ad56a21c9
raju6991 <techwithrazu@gmail.com>
2026-04-13 19:36:50 +1000

    refactor config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,28 +5,39 @@ config({
     path: path.join(process.cwd(), `.env.${process.env.NODE_ENV || 'dev'}`),
 })
 
-const {
-    PORT,
-    NODE_ENV,
-    DB_HOST,
-    DB_PORT,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME,
-    REFRESH_TOKEN_SECRET,
-    JWKS_URI,
-    PRIVATE_KEY,
-} = process.env
+const getEnv = (key: string): string | undefined => {
+    return process.env[key]
+}
 
 export const Config = {
-    PORT,
-    NODE_ENV,
-    DB_HOST,
-    DB_PORT,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME,
-    REFRESH_TOKEN_SECRET,
-    JWKS_URI,
-    PRIVATE_KEY,
+    get PORT() {
+        return getEnv('PORT')
+    },
+    get NODE_ENV() {
+        return getEnv('NODE_ENV')
+    },
+    get DB_HOST() {
+        return getEnv('DB_HOST')
+    },
+    get DB_PORT() {
+        return getEnv('DB_PORT')
+    },
+    get DB_USERNAME() {
+        return getEnv('DB_USERNAME')
+    },
+    get DB_PASSWORD() {
+        return getEnv('DB_PASSWORD')
+    },
+    get DB_NAME() {
+        return getEnv('DB_NAME')
+    },
+    get REFRESH_TOKEN_SECRET() {
+        return getEnv('REFRESH_TOKEN_SECRET')
+    },
+    get JWKS_URI() {
+        return getEnv('JWKS_URI')
+    },
+    get PRIVATE_KEY() {
+        return getEnv('PRIVATE_KEY')
+    },
 }
